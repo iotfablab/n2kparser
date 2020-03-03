bcd9d2ca51fed7dbd3809ec06b1f8f519cd5fa4b
Shantanoo Desai <des@biba.uni-bremen.de>
2020-03-03 17:41:16 +0100

    Bump v1.3 -> v1.4

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
-setup(name='n2kparser',
-    version=1.3,
-    description='Extract values from Actisense-Serial PGNs and save them to InfluxDB',
+setup(
+    name='n2kparser',
+    version=1.4,
+    description='Extract values from Actisense-Serial PGNs and save them to InfluxDB and publish them to MQTT',
     url='https://github.com/iotfablab/n2kparser',
     author='Shantanoo Desai',
     author_email='des@biba.uni-bremen.de',
@@ -10,7 +11,8 @@ setup(name='n2kparser',
     packages=['n2kparser'],
     scripts=['bin/n2kparser'],
     install_requires=[
-        'influxdb'
+        'influxdb',
+        'paho-mqtt'
     ],
     include_data_package=True,
     zip_safe=False
